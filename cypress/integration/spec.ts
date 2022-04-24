41287175dcdb1d7c3dd96a80fbbda7ef7a7e9a4f
rikinhim <rikinhim@gmail.com>
2022-04-24 16:42:15 -0400

    add cypress

---
+++ cypress/integration/spec.ts
@@ -0,0 +1,10 @@
+// enables intelligent code completion for Cypress commands
+// https://on.cypress.io/intelligent-code-completion
+/// <reference types="cypress" />
+
+describe('Hello world test', () => {
+  it('shows learn link', function () {
+    cy.visit('http://localhost:3000')
+    cy.get('a').should('have.text', 'hello world!')
+  })
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function App() {
-  return <div>hello world!</div>;
+  return <a href="http://google.com">hello world!</a>;
 }
 
 export default App;
